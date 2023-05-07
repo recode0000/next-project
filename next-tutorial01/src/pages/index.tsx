f53e3078d7e096af2e12fe59e970a3c4d47b8e71
Rikito Ikeda <rikito1036@icloud.com>
2023-05-07 20:02:07 +0900

    [add] Layout コンポーネント

--- next-tutorial01/src/pages/index.tsx
+++ next-tutorial01/src/pages/index.tsx
@@ -4,10 +4,11 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <div>
+
     <Head>
       <title>トップページ</title>
-      <meta name="description" content="" />
-      <meta property="og:title" content="" />
+      <meta name="description" content="これはトップページです" />
+      <meta property="og:title" content="トップページ" />
       <meta property="og:description" content="これはトップページ" />
     </Head>
       <ul>
