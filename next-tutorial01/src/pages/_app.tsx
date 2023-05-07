ef0cd5a7ec4a24a34c1af836cd248c838bbe6dd0
Rikito Ikeda <rikito_ikeda@commude.co.jp>
2023-05-07 19:54:20 +0900

    [update] メタ情報設定 Next/Head

--- next-tutorial01/src/pages/_app.tsx
+++ next-tutorial01/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 
+import Layout from '@/components/layout';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  );
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
--- next-tutorial01/src/pages/about.tsx
+++ next-tutorial01/src/pages/about.tsx
@@ -1,9 +1,10 @@
-import Layout from "@/components/layout";
-
+import Layout from '../components/layout';
 export default function About() {
   return (
-    <Layout>
-      <h1>About Page</h1>
-    </Layout>
+  <h1>About Page</h1>
   );
 }
+
+About.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>
+};
