d6cf2b5b73c666304fcce177402f668c505d3689
Rikito Ikeda <rikito1036@icloud.com>
2023-05-11 08:29:38 +0900

    [fix] 調整

--- next-tutorial01/src/pages/posts/index.jsx
+++ next-tutorial01/src/pages/posts/index.jsx
@@ -21,6 +21,5 @@ export default function index({posts}) {
 export async function getStaticProps() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts = await res.json();
-  console.log(posts);
   return { props: { posts } };
 }
