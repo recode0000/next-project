ec451203cce151e754be343769008838d4197b36
Rikito Ikeda <rikito1036@icloud.com>
2023-05-28 14:18:01 +0900

    [fix] 削除&

--- next-tutorial01/src/pages/_app.jsx
+++
@@ -1,8 +0,0 @@
-
-import Layout from '@/components/layout';
-import '../styles/globals.css';
-
-export default function MyApp({ Component, pageProps }) {
-  const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
-}
